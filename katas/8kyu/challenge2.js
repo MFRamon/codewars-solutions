33b223134cc9324b2f4c66ef2a7f286ea9df15cb
MFRamon <ramon.manfig@gmail.com>
2023-01-26 11:44:57 -0600

    Adding Solution of the day 26 January

---
+++ katas/8kyu/challenge2.js
@@ -0,0 +1,13 @@
+// INSTRUCTIONS 
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: 
+// Only valid inputs will be given.
+
+
+// Resolved January 26th 2023
+// By Ramon Manrique Figueroa
+
+function booleanToString(b){
+    return b ? 'true' : 'false';
+}
